76c908eda1fa8f3e5cbe91cf574c801d6cd97e7d
Niklas Hohenwarter <info@niklashohenwarter.com>
2016-04-21 12:40:48 +0200

    added db

--- src/database.py
+++ src/database.py
@@ -15,6 +15,40 @@ class Database():
         self.classes = Base.classes
         self.wahltermin = "2015-10-11"
 
+    def save_to_db(self, data, statusbarinstance):
+        if not self.validate_data_header(data[0]):
+            return False
+
+        #self.session.execute("DELETE FROM parteistimmen")
+        #self.session.execute("DELETE FROM sprengel")
+        Sprengel = self.classes.sprengel
+        Parteistimmen = self.classes.parteistimmen
+        parteien = data[0][9:]
+        counter = 1
+        for row in data[1:]:
+            statusbarinstance.showMessage("Processing "+str(counter)+"/"+str(len(data[1:]))+". Please wait...", 2000)
+            t = row[1]
+            wv = row[2]
+            wk = row[3]
+            bz = row[4]
+            spr = row[5]
+            wber = row[6]
+            abg = row[7]
+            ung = row[8]
+            part = row[9:]
+            self.session.add(Sprengel(snummer=spr,wahlberechtigt=wber,abgegeben=abg,ungueltig=ung,bnummer=bz,termin=self.wahltermin))
+            for x in range(0,len(part)):
+                anzahl = part[x]
+                partei = parteien[x]
+                self.session.add(Parteistimmen(stimmenanzahl=anzahl,pbez=partei,snummer=spr,bnummer=bz,termin=self.wahltermin))
+            counter = counter +1
+            try:
+                self.session.commit()
+            except:
+                pass
+        statusbarinstance.showMessage("Saved to Database!", 2000)
+        return True
+
     def read_from_db(self, statusbarinstance):
         query = "SELECT sprengel.bnummer,sprengel.snummer,sprengel.wahlberechtigt,sprengel.abgegeben,sprengel.ungueltig, bezirk.wknummer"\
             " FROM sprengel INNER JOIN bezirk ON bezirk.bnummer = sprengel.bnummer WHERE termin=\"%s\";" % (self.wahltermin)
@@ -42,40 +76,6 @@ class Database():
             counter = counter +1
         return data
 
-    def save_to_db(self, data, statusbarinstance):
-        if not self.validate_data_header(data[0]):
-            return False
-        #self.session.execute("DELETE FROM parteistimmen")
-        #self.session.execute("DELETE FROM sprengel")
-        Sprengel = self.classes.sprengel
-        Parteistimmen = self.classes.parteistimmen
-        parteien = data[0][9:]
-        counter = 1
-        for row in data[1:]:
-            statusbarinstance.showMessage("Processing "+str(counter)+"/"+str(len(data[1:]))+". Please wait...", 2000)
-            t = row[1]
-            wv = row[2]
-            wk = row[3]
-            bz = row[4]
-            spr = row[5]
-            wber = row[6]
-            abg = row[7]
-            ung = row[8]
-            part = row[9:]
-            self.session.add(Sprengel(snummer=spr,wahlberechtigt=wber,abgegeben=abg,ungueltig=ung,bnummer=bz,termin=self.wahltermin))
-            for x in range(0,len(part)):
-                anzahl = part[x]
-                partei = parteien[x]
-                self.session.add(Parteistimmen(stimmenanzahl=anzahl,pbez=partei,snummer=spr,bnummer=bz,termin=self.wahltermin))
-            counter = counter +1
-            try:
-                self.session.commit()
-            except:
-                return False
-        statusbarinstance.showMessage("Saved to Database!", 2000)
-        return True
-
-
     def validate_data_header(self, header):
         if not "T" in header[1] or not "WV" in header[2] or not "WK" in header[3] or not "BZ" in header[4] or \
         not "SPR" in header[5] or not "WBER" in header[6] or not "ABG" in header[7] or not "UNG" in header[8]:
@@ -86,8 +86,9 @@ class Database():
     def generateProjection(self, statusbarinstance):
         try:
             self.session.execute("CALL erzeugeHochrechnung(\"%s\", \"%s\")" % (self.wahltermin, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+            self.session.commit()
         except:
-            return False
+            pass
 
         statusbarinstance.showMessage("Reading latest projection...",2000)
         data = []
